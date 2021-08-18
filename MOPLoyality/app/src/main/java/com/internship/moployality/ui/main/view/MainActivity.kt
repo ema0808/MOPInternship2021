7ebf0ca9a938f0b8d1983b2560c03c0ce57c8757
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-08-18 10:37:39 +0200

    Splash screen

--- MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/MainActivity.kt
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/MainActivity.kt
@@ -1,78 +1,31 @@
 package com.internship.moployality.ui.main.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.Observer
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.Navigation
 import com.internship.moployality.R
-import com.internship.moployality.data.api.ApiHelper
-import com.internship.moployality.data.api.ApiServiceImpl
-import com.internship.moployality.data.model.User
-import com.internship.moployality.ui.base.ViewModelFactory
-import com.internship.moployality.ui.main.adapter.MainAdapter
-import com.internship.moployality.ui.main.viewmodel.MainViewModel
-import com.internship.moployality.utils.Status
-import kotlinx.android.synthetic.main.activity_main.progressBar
-import kotlinx.android.synthetic.main.activity_main.recyclerView
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var mainViewModel: MainViewModel
-  private lateinit var adapter: MainAdapter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setupUI()
-    setupViewModel()
-    setupObserver()
+    supportActionBar?.hide()
+
+    Handler(Looper.getMainLooper()).postDelayed({
+//      val intent= Intent(this@MainActivity,HomeActivity::class.java)
+//      startActivity(intent)
+      Navigation.findNavController(this, R.id.my_nav).navigate(R.id.homeActivity)
+      finish()
+    },3000)
   }
 
   private fun setupUI() {
-    recyclerView.layoutManager = LinearLayoutManager(this)
-    adapter = MainAdapter(arrayListOf())
-    recyclerView.addItemDecoration(
-      DividerItemDecoration(
-        recyclerView.context,
-        (recyclerView.layoutManager as LinearLayoutManager).orientation
-      )
-    )
-    recyclerView.adapter = adapter
+
   }
 
-  private fun setupObserver() {
-    mainViewModel.getUsers().observe(this, Observer {
-      when (it.status) {
-        Status.SUCCESS -> {
-          progressBar.visibility = View.GONE
-          it.data?.let { users -> renderList(users) }
-          recyclerView.visibility = View.VISIBLE
-        }
-        Status.LOADING     -> {
-          progressBar.visibility = View.VISIBLE
-          recyclerView.visibility = View.GONE
-        }
-        Status.ERROR -> {
-          //Handle Error
-          progressBar.visibility = View.GONE
-          Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-        }
-      }
-    })
-  }
-
-  private fun renderList(users: List<User>) {
-    adapter.addData(users)
-    adapter.notifyDataSetChanged()
-  }
-
-  private fun setupViewModel() {
-    mainViewModel = ViewModelProviders.of(
-      this,
-      ViewModelFactory(ApiHelper(ApiServiceImpl()))
-    ).get(MainViewModel::class.java)
-  }
 }
