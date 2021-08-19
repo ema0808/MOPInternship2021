401786b370e5367e4b9132846a0b9082fe48b070
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-08-19 14:23:32 +0200

    Code update for Log in

--- MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/HomeActivity.kt
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/HomeActivity.kt
@@ -1,13 +1,36 @@
 package com.internship.moployality.ui.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+
 import com.internship.moployality.R
 
+
 class HomeActivity : AppCompatActivity() {
+
+     var mGoogleSignInClient: GoogleSignInClient? =null
+     var singin: SignInButton?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+      // findViewById<SignInButton> (R.id.sign_in_button).setOnClickListener(this);
 
+        singin =findViewById(R.id.sign_in_button)
+        singin.setOnClickListener {  }
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
+
 }
--- MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/MainActivity.kt
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/MainActivity.kt
@@ -4,17 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.internship.moployality.R
+import com.internship.moployality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+   var  binding: ActivityMainBinding?=null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
     setupUI()
     supportActionBar?.hide()
+
+      binding= ActivityMainBinding.inflate(layoutInflater)
+      val view: View = binding!!.root
+      setContentView(view)
+
+      //binding.
 
     Handler(Looper.getMainLooper()).postDelayed({
 //      val intent= Intent(this@MainActivity,HomeActivity::class.java)
