58b040a5eed0c6946daa484c7fa0942947de49c8
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-08-20 10:22:52 +0200

    Updated Home Activity

---
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.internship.moployality.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.internship.moployality.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
--- MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/HomeActivity.kt
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/HomeActivity.kt
@@ -1,26 +1,37 @@
 package com.internship.moployality.ui.main.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
 
-import com.internship.moployality.R
+
+
 
 
 class HomeActivity : AppCompatActivity() {
 
      var mGoogleSignInClient: GoogleSignInClient? =null
      var singin: SignInButton?=null
+     val RC_SIGN_IN=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(com.internship.moployality.R.layout.activity_home)
       // findViewById<SignInButton> (R.id.sign_in_button).setOnClickListener(this);
 
-        singin =findViewById(R.id.sign_in_button)
-        singin.setOnClickListener {  }
+        singin =findViewById(com.internship.moployality.R.id.sign_in_button)
+        singin.setOnClickListener {
+
+            when (view.getId()) {
+                com.internship.moployality.R.id.sign_in_button -> signIn()
+            } }
 
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -32,5 +43,34 @@ class HomeActivity : AppCompatActivity() {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            val intent= Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.statusCode)
+
+        }
+    }
 }
