fcb23aa478685bf5c1ae6f69c7a7ff04716a2bb5
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-09-20 11:43:43 +0200

    Update with crash error

--- MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/DashboardActivity.kt
+++ MOPLoyality/app/src/main/java/com/internship/moployality/ui/main/view/DashboardActivity.kt
@@ -3,19 +3,26 @@ package com.internship.moployality.ui.main.view
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.internship.moployality.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+
+        bottomNavigationView.setupWithNavController(navController)
 
 //        imageVIew= findViewById<>(R.id.imageView2)
 //        name= findViewById<>(R.id.textName)
